e893d03447ea17a4b714e525ce84a9a1602307e6
aboublini <p19117@unipi.gr>
2023-05-01 19:44:56 +0300

    home screen - design first try

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './HomeComponentStyle.css'
 import homeVideo from '../visual-material/HomeBackround.mp4';
 import Logo from "../visual-material/logo.png";
+import lifeImage from "../visual-material/LifeBackround.jpg";
+import homeImage from "../visual-material/HomeBackround.jpg";
 
 const HomeComponent = () => {
     return (
         <div className="container">
-            <video autoPlay loop muted id="video">
-                <source src={homeVideo} type="video/mp4"/>
-            </video>
+            {/*<video autoPlay loop muted id="video">*/}
+            {/*    <source src={homeVideo} type="video/mp4"/>*/}
+            {/*</video>*/}
+
+            <img src={homeImage} id="home-image" alt="life-background"/>
+            <div className="img-front"></div>
             <div className="content">
                 <div className="welcome">
                     <img src={Logo} className="home-img" alt=""/>
