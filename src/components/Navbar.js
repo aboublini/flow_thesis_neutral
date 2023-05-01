54a5d0c162444f7af798ff1cc79eae97548a495e
aboublini <p19117@unipi.gr>
2023-05-01 19:04:48 +0300

    navbar style and responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,56 +10,62 @@ const Navbar = () => {
     const [Click, setClick] = useState(false);
     const handleClick = () => setClick(!Click);
 
-    // Change navbar color when scrolling
-    const [color, setColor] = useState(false);
-    const changeColor = () => {
-        if (window.scrollY >= 90) {
-            setColor(true);
-        } else {
-            setColor(false);
-        }
-    }
-
-    window.addEventListener('scroll', changeColor);
+    // // Change navbar color when scrolling
+    // const [color, setColor] = useState(false);
+    // const changeColor = () => {
+    //     if (window.scrollY >= 90) {
+    //         setColor(true);
+    //     } else {
+    //         setColor(false);
+    //     }
+    // }
+    //
+    // window.addEventListener('scroll', changeColor);
 
     return (
-        <div className={color ? 'header header-bg' : 'header'}>
-            <Link to="/home"><img src={Logo} className="nav-logo" alt=""/></Link>
-            <ul className={Click ? "nav-menu active" : "nav-menu"}>
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-                <li>
-                    <Link to="/daily">Daily Goals</Link>
-                </li>
-                <li>
-                    <Link to="/life">Budget Manager</Link>
-                </li>
-                <li>
-                    <Link to="/notes">Notes</Link>
-                </li>
-                <li className="dropdown">
-                    <button className="dropbtn">More</button>
-                    <div className="dropdown-content">
+        <div className="header">
+            <div className="left-navbar">
+                <Link to="/home"><img src={Logo} className="nav-logo" alt=""/></Link>
+                <ul className={Click ? "nav-menu active" : "nav-menu"}>
+                    <li>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/daily">Daily Goals</Link>
+                    </li>
+                    <li>
+                        <Link to="/life">Budget Manager</Link>
+                    </li>
+                    <li>
+                        <Link to="/notes">Notes</Link>
+                    </li>
+                    {/*<li className="dropdown">*/}
+                    {/*    <button className="dropbtn">More</button>*/}
+                    {/*    <div className="dropdown-content">*/}
+                    {/*        <Link to="/help">FAQs</Link>*/}
+                    {/*        <Link to="/about">About Flow</Link>*/}
+                    {/*        <Link to="/">Logout</Link>*/}
+                    {/*    </div>*/}
+                    {/*</li>*/}
+                    <li className="burger">
                         <Link to="/help">FAQs</Link>
+                    </li>
+                    <li className="burger">
                         <Link to="/about">About Flow</Link>
+                    </li>
+                    <li className="burger">
                         <Link to="/">Logout</Link>
-                    </div>
-                </li>
-                <li className="burger">
-                    <Link to="/help">FAQs</Link>
-                </li>
-                <li className="burger">
-                    <Link to="/about">About Flow</Link>
-                </li>
-                <li className="burger">
-                    <Link to="/">Logout</Link>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="right-navbar">
+                <li className="help-button"><Link to="/about">HELP</Link></li>
+                <li className="logout-button"><Link to="/">LOGOUT</Link></li>
+            </div>
+
             <div className="hamburger" onClick={handleClick}>
-                {Click ? (<FaTimes size={20} style={{color: '#000'}}/>) : (<FaBars size={20} style={{color: '#000'}}/>)}
-
-
+                {Click ? (<FaTimes size={20} style={{color: '#000'}}/>) : (<FaBars size={20} style={{color: '#6069FA'}}/>)}
             </div>
         </div>
     );
